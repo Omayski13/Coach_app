2ab1a3acd5a73dab70ae171c4d4618e1036f3867
Omayski13 <k.omayski@abv.bg>
2024-12-10 01:16:17 +0200

    fix: updated home page html and view

--- coach_app/common/views.py
+++ coach_app/common/views.py
@@ -20,6 +20,7 @@ class HomePageView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['drills'] = Drill.objects.all()
+        return context
 
     def get_object(self):
         drill = Drill.objects.filter(approved=True, graphics__isnull=False).first()
