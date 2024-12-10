f484df93d13248f0326e73a61dd30706cb0c0bd9
Omayski13 <k.omayski@abv.bg>
2024-12-10 13:46:55 +0200

    updated drills dash

--- tests/drills/view/test_delete_view.py
+++ tests/drills/view/test_delete_view.py
@@ -11,37 +11,29 @@ UserModel = get_user_model()
 
 class DrillDeleteViewTests(TestCase):
 
-
-
     def setUp(self):
-
-        # Create a test user
         self.user = UserModel.objects.create_user(
             username='testuser',
             email='testemail@mail.com',
             password='password'
         )
 
-        # Create a drill instance with a mock image
         self.drill = Drill.objects.create(
             name='Test Drill',
             objectives='Test objectives',
             description='Test description',
-            focus='Technical',  # Assuming 'FocusChoices' contains 'Technical'
+            focus='Technical',
             dimensions='5x5',
             series='Series A',
             author=self.user,
             approved=True,
-            graphics=None  # Attach the mock image
+            graphics=None
         )
 
-        # Authenticate the user
         self.client.login(username='testuser', password='password')
 
 
-
     def test_drill_delete_view_permission(self):
-        # Test if a user who is not the owner of the drill is prevented from deleting it
         another_user = UserModel.objects.create_user(
             username='anotheruser',
             email='anotheremail@mail.com',
@@ -52,14 +44,11 @@ class DrillDeleteViewTests(TestCase):
         url = reverse('drill-delete', args=[self.drill.pk])
         response = self.client.get(url)
 
-        # The response should be a 403 Forbidden (user does not have permission)
         self.assertEqual(response.status_code, 403)
 
 
     def test_drill_delete_view_invalid_drill(self):
-        # Test if a non-existent drill gives a 404 error
         url = reverse('drill-delete', args=[999])  # assuming 999 doesn't exist
         response = self.client.get(url)
 
-        # Check for a 404 error when the drill does not exist
         self.assertEqual(response.status_code, 404)
